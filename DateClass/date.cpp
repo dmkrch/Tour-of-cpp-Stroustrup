fde195ceb898554d81a4ddbc07b4a76fa51d62f0
Dmitriy <dmkrch1@gmail.com>
2021-10-08 17:56:26 +0300

    added class Year to Date

--- DateClass/date.cpp
+++ DateClass/date.cpp
@@ -24,22 +24,39 @@ std::ostream& operator<<(std::ostream& os, Month m)
 
 
 
+class Year 
+{
+public:
+    class InvalidYear{};
+    Year(int x) : year{x} { if (x<min || max<=x) throw InvalidYear(); }
+    int get_year() { return year; }
+private:
+    static const int min = 1800;
+    static const int max = 2200;
+    int year;
+};
+
+std::ostream& operator<<(std::ostream& os, Year y)
+{
+    return os << y.get_year();
+}
+
 /* -------------------------DATE CLASS REALISATION------------------------------------- */
 class Date
 {
 public:
     class InvalidDate {};
-    Date(int y, Month m, int d);
+    Date(Year y, Month m, int d);
     void ShowDate();
     Month get_month() { return month; }
 private:
-    int year;
+    Year year;
     Month month;
     int day;
     bool is_valid();
 };
 
-Date::Date(int y, Month m, int d)
+Date::Date(Year y, Month m, int d)
     : year{y}, month{m}, day{d}
 {
     if (!is_valid()) throw InvalidDate(); // if date is not valid - throw exception
@@ -48,7 +65,6 @@ Date::Date(int y, Month m, int d)
 bool Date::is_valid()
 {
     if ((int)month < 1 || (int)month > 12) return false;
-    if (year < 0) return false;
     if (day < 1 || day > 31) return false;
     return true;
 }
@@ -71,7 +87,7 @@ int main()
 {
     try 
     {
-        Date d1 = {2001, Month::Jan, 25};
+        Date d1 = {Year{2001}, Month::Jan, 25};
         d1.ShowDate();
     }
     catch(Date::InvalidDate d)
