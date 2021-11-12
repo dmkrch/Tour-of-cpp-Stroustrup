24932937e7bfd8016669c39abb661986e8fd5b54
Dmitriy <dmkrch1@gmail.com>
2021-11-12 15:32:57 +0300

    copy constructor for vector added

--- ch18/initializer_list.cpp
+++ ch18/initializer_list.cpp
@@ -6,21 +6,33 @@ private:
     int sz;
     double* elem;
 public:
+    // default constructor
     MyVector(int s) : sz{s}, elem{new double[sz]} {
         for (int i = 0; i < static_cast<int>(sz); ++i)
             elem[i] = 0.0;
     }
 
+    //constructor with initializer list
     MyVector(std::initializer_list<double> lst)
     : sz{static_cast<int>(lst.size())}, elem{new double[sz]} {
         std::copy(lst.begin(), lst.end(), elem);
     }
 
+    MyVector(const MyVector& arg) 
+    : sz{arg.sz}, elem{new double[arg.sz]} {
+        std::cout << "copy constructor has been called for vector\n";
+        for (int i = 0; i < sz; ++i) 
+            elem[i] = arg.elem[i];
+    }
+
+    // function that prints elements of vector
     void PrintVector() {
         for (int i = 0; i < sz; ++i) {
             std::cout << elem[i] << " ";
         }
     }
+
+
 };
 
 
@@ -35,6 +47,12 @@ int main() {
         MyVector v1(6);
         v1.PrintVector();
          // 0 0 0 0 0 0
+
+        v1 = v; // not copy constructor
+        MyVector v2{v1}; // copy constructor
+        
+        MyVector v3(3);
+        v3 = v1; // not copy constructor
     }
     catch(...) {
         std::cout << "some general error" << std::endl;
